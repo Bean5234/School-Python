2cec35c70899726656ebded8e4964bc721870bf3
Bean5234 <204515@novacollege.nl>
2026-01-29 09:56:58 +0100

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,32 @@
+naam = "Dean"
+leeftijd = 16
+opleiding = "Software devolper"
+verjaardag = 18
+slber = "J.Zonneveld"
+bench_press_PR = 60
+banksaldo = 2000.58
+print ("naam:" + naam)
+print ("leeftijd:" + str(leeftijd))
+print ("opleiding:" + opleiding)
+print ("verjaardag:" + str(verjaardag) , "OKT")
+print ("slber:" + slber)
+print ("bench press PR:" + str(bench_press_PR) , "KG")
+print ("bankrekening:" + "€" + str(banksaldo))
+
+
+mijn_getal = 4
+print(mijn_getal)
+print(mijn_getal * 2)
+print(mijn_getal **2)
+print("hallo " *4)
+print(mijn_getal /3 )
+print(mijn_getal //3)
+print(mijn_getal %3)
+mijn_getal = 16
+print(mijn_getal)
+print(mijn_getal * 2)
+print(mijn_getal **2)
+print("hallo " *16)
+print(mijn_getal / 3)
+print(mijn_getal //3)
+print(mijn_getal %3)
